dd864e7e8e072e46aed56f5cb898fbf4c41cab41
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-08-10 19:07:56 +0530

    Added API

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
+const products = require("./utils/productData");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.write("Hello world!")
-  response.end();
+  if (request.method === "GET" && request.url === "/api/products") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(products)); //send data
+  } else {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: "Data Not found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
