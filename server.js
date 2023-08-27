bc541c7b6bea671a7b23321510cf33bba30b1634
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-08-27 15:32:20 +0530

    Added API Versioning

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ const {
 } = require("./controllers/productController");
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/api/products" && request.method === "GET") {
+  if (request.url === "/api/v1/products" && request.method === "GET") {
     getProducts(request, response);
   } else if (
-    request.url.match(/\/api\/products\/([0-9]+)/) &&
+    request.url.match(/\/api\/v1\/products\/([0-9]+)/) &&
     request.method === "GET"
   ) {
     const id = request.url.split("/").pop();
     getProductById(request, response, id);
-  } else if (request.url === "/api/products" && request.method === "POST") {
+  } else if (request.url === "/api/v1/products" && request.method === "POST") {
     createProduct(request, response);
   } else if (
-    request.url.match(/\/api\/products\/([0-9]+)/) &&
+    request.url.match(/\/api\/v1\/products\/([0-9]+)/) &&
     request.method === "PUT"
   ) {
     const id = request.url.split("/").pop();
     updateProduct(request, response, id);
   } else if (
-    request.url.match(/\/api\/products\/([0-9]+)/) &&
+    request.url.match(/\/api\/v1\/products\/([0-9]+)/) &&
     request.method === "DELETE"
   ) {
     const id = request.url.split("/").pop();
